a71b6467cd5c7e1b0d4b7b514763b18adeadd896
Tiago Hermano <tiagofhermano@gmail.com>
2017-02-10 11:50:02 -0200

    Add terceiro app: ATM Consultoria: 1 navigation controller e 4 view controllers

---
+++ Segues/Segues/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Segues
+//
+//  Created by Dev12 on 10/02/17.
+//  Copyright © 2017 Tiago-Dev13. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
