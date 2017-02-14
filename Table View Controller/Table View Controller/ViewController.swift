5645e9f9d899913474215dc39d434d5d88a33687
Tiago Hermano <tiagofhermano@gmail.com>
2017-02-14 11:11:28 -0200

    Add projeto que exemplifica o uso de table views

--- Table View Controller/Table View Controller/ViewController.swift
+++ Table View Controller/Table View Controller/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var dados: [String] = ["Lasanha", "Pizza", "Torta"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // Método responsável por informar a quantidade de seções da tabela
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    // Método responsável por informar a quantidade itens(células) da tabela
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dados.count
+    }
+    
+    // Método responsável pela montagem de cada célula
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celulaReuso = "celulaReuso"
+        let celula = tableView.dequeueReusableCell(withIdentifier: celulaReuso, for: indexPath)
+        celula.textLabel?.text = dados[ indexPath.row ]
+        
+        return celula
+    }
 }
 
