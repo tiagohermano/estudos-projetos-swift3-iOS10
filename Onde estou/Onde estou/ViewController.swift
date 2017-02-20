41a5fa66e30dca455b0c12fc17385dea1ad1a51f
Tiago Hermano <tiagofhermano@gmail.com>
2017-02-20 09:25:17 -0300

    App finalizado. Utilizado conceitos da utilização de GPS aprendidos no projeto: Trabalhando com mapas e introduzido alguns conceitos novos, como: Recuperação de dados de endereço a partir de uma coordenada de posicionamento

--- Onde estou/Onde estou/ViewController.swift
+++ Onde estou/Onde estou/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     @IBOutlet weak var mapa: MKMapView!
     var gerenciadorLocalizacao = CLLocationManager()
     
+    @IBOutlet weak var velocidadeLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var enderecoLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,27 +30,118 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         
     }
     
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let localizacaoUsuario = locations.last
+        
+        let latitude = localizacaoUsuario!.coordinate.latitude
+        let longitude = localizacaoUsuario!.coordinate.longitude
+        let localizacao: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        // Preenche as labels
+        latitudeLabel.text = String(describing: latitude)
+        longitudeLabel.text = String(describing: longitude)
+        
+        if localizacaoUsuario!.speed > 0 {
+            velocidadeLabel.text = String(localizacaoUsuario!.speed)
+        }
+        
+        // Zoom da visualização no mapa
+        let deltaLatitude: CLLocationDegrees = 0.08
+        let deltaLongitude: CLLocationDegrees = 0.08
+        let areaVisualizacao: MKCoordinateSpan = MKCoordinateSpanMake(deltaLatitude, deltaLongitude)
+        
+        // Monta exibição do mapa
+        let regiao: MKCoordinateRegion = MKCoordinateRegionMake(localizacao, areaVisualizacao)
+        
+        mapa.setRegion(regiao, animated: true)
+        
+        CLGeocoder().reverseGeocodeLocation(localizacaoUsuario!) { (detalhesLocal, erro) in
+            if erro == nil {
+                if let dadosLocal = detalhesLocal?.first {
+                    
+                    // Rua
+                    var thoroughfare = ""
+                    if dadosLocal.thoroughfare != nil {
+                        thoroughfare = dadosLocal.thoroughfare!
+                    }
+                    
+                    // Numero
+                    var subThoroughfare = ""
+                    if dadosLocal.subThoroughfare != nil {
+                        subThoroughfare = dadosLocal.subThoroughfare!
+                    }
+                    
+                    // Cidade
+                    var locality = ""
+                    if dadosLocal.locality != nil {
+                        locality = dadosLocal.locality!
+                    }
+                    
+                    // Bairro
+                    var subLocality = ""
+                    if dadosLocal.subLocality != nil {
+                        subLocality = dadosLocal.subLocality!
+                    }
+                    
+                    // CEP
+                    var postalCode = ""
+                    if dadosLocal.postalCode != nil {
+                        postalCode = dadosLocal.postalCode!
+                    }
+                    
+                    // País
+                    var country = ""
+                    if dadosLocal.country != nil {
+                        country = dadosLocal.country!
+                    }
+                    
+                    var admnistrativeArea = ""
+                    if dadosLocal.administrativeArea != nil {
+                        admnistrativeArea = dadosLocal.administrativeArea!
+                    }
+                    
+                    var subAdministrativeArea = ""
+                    if dadosLocal.subAdministrativeArea != nil {
+                        subAdministrativeArea = dadosLocal.subAdministrativeArea!
+                    }
+                    
+                    self.enderecoLabel.text = thoroughfare + " - " +
+                                              subThoroughfare + " - " +
+                                              locality + " - " +
+                                              country
+                    
+                } else {
+                // exibir alert de erro
+                }
+            }
+        }
+    }
+    
     // Caso a permissao de localizacao seja negada, usado este tratamento.
     // Que exibe um alerta com duas acoes: Abrir tela de configuracoes do iPhone e Cancelar.
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if status != .authorizedWhenInUse {
-            var alertController = UIAlertController(title: "Permissao de localizacao", message: "Necessário permissao para acesso a sua localizacao! Por favor Habilite.", preferredStyle: .alert)
+            let alertController = UIAlertController(title: "Permissao de localizacao", message: "Necessário permissao para acesso a sua localizacao! Por favor Habilite.", preferredStyle: .alert)
             
             //
-            var acaoConfiguracoes = UIAlertAction(title: "Abrir Configuracoes", style: .default, handler: {(alertaConfiguracoes) in
+            let acaoConfiguracoes = UIAlertAction(title: "Abrir Configuracoes", style: .default, handler: {(alertaConfiguracoes) in
                 if let configuracoes = NSURL(string: UIApplicationOpenSettingsURLString) {
                     UIApplication.shared.open( configuracoes as URL)
                 }
             })
             
-            var acaoCancelar = UIAlertAction(title: "Cancelar", style: .default, handler: nil)
+            let acaoCancelar = UIAlertAction(title: "Cancelar", style: .default, handler: nil)
             alertController.addAction(acaoConfiguracoes)
             alertController.addAction(acaoCancelar)
             
             present(alertController, animated: true, completion: nil)
         }
     }
+    
+    func locationManagerDidPauseLocationUpdates(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
 
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
