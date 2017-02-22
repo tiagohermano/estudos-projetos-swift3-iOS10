2a7170722230ff589459d36640dc3723bb1e2077
Tiago Hermano <tiagofhermano@gmail.com>
2017-02-22 16:47:56 -0300

    Exclusão de itens através do Core Data

--- Core Data/Core Data/ViewController.swift
+++ Core Data/Core Data/ViewController.swift
@@ -20,8 +20,9 @@ class ViewController: UIViewController {
         // gerenciador de persistencia de dados utilizando o Core Data
         let context = appDelegate.persistentContainer.viewContext
         
-        /*
         let produto = NSEntityDescription.insertNewObject(forEntityName: "Produtos", into: context)
+        /*
+        
         
         // Configurar objeto
         produto.setValue("Ovos", forKey: "nome")
@@ -51,11 +52,11 @@ class ViewController: UIViewController {
         // let ordenacaoZA = NSSortDescriptor(key: "preco", ascending: false)
         
         //  Consultas no Banco de Dados
-        //let predicate = NSPredicate(format: "nome == %@", "Manteiga")
+        //let predicate = NSPredicate(format: "nome == %@", "Requeijão")
         //let predicate = NSPredicate(format: "cor contains %@", "osa")
         //let predicate = NSPredicate(format: "cor contains [c] %@", "rosa")
         //let predicate = NSPredicate(format: "nome beginswith [c] %@", "Le")
-        //let filtroPreco = NSPredicate(format: "preco >= @%", "4.0")
+        let filtroPreco = NSPredicate(format: "preco <= @%", "4.0")
         //let filtroNome = NSPredicate(format: "nome == @% ", "Manteiga")
         
         //  Combinando consultas
@@ -64,16 +65,40 @@ class ViewController: UIViewController {
         
         // Aplicar filtros e consultas
         requisicao.sortDescriptors = [ordenacaoAZ]
-        // requisicao.predicate = combinacaoFiltros
+        requisicao.predicate = filtroPreco
         
         do {
             let produtos = try context.fetch(requisicao)
             if produtos.count > 0 {
-                for produto in produtos {
+                for produto in produtos as! [NSManagedObject]{
+                    
+                    // Atualizar itens
+                    //(produto as AnyObject).setValue("Requeijão", forKey: "nome")
+                    //(produto as AnyObject).setValue(5.0, forKey: "preco")
+                    
+                    /*
+                    do{
+                        try context.save()
+                        print("Dados salvos com sucesso")
+                    } catch {
+                        print("Não foi possível salvar o novo valor")
+                    }
+                    */
+                    
+                    // Deletar itens
+                    context.delete(produto)
+                    do{
+                        try context.save()
+                        print("Item removido com sucesso")
+                    } catch {
+                        print("Não foi possível remover item")
+                    }
+                    
+                    // Recuperar itens
                     let nomeProduto = (produto as AnyObject).value(forKey: "nome")
                     let precoProduto = (produto as AnyObject).value(forKey: "preco")
                     
-                    print(String(describing: nomeProduto) + "-" + String(describing: precoProduto))
+                    print(String(describing: nomeProduto) + " - " + String(describing: precoProduto))
                 }
             } else {
                 print("Nenhum produto encontrado")
