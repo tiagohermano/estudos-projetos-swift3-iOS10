132a158c9f281bd64c50e2241bcd5428e3713ce5
Tiago Hermano <tiagofhermano@gmail.com>
2017-02-22 15:53:22 -0300

    Aplicando filtros e listando dados de diferentes modos através do Core Data

--- Core Data/Core Data/ViewController.swift
+++ Core Data/Core Data/ViewController.swift
@@ -21,15 +21,13 @@ class ViewController: UIViewController {
         let context = appDelegate.persistentContainer.viewContext
         
         /*
-        //    Como salvar dados utilizando o Core Data
-        
-        let usuario = NSEntityDescription.insertNewObject(forEntityName: "Usuarios", into: context)
+        let produto = NSEntityDescription.insertNewObject(forEntityName: "Produtos", into: context)
         
         // Configurar objeto
-        usuario.setValue("teste", forKey: "loginUsuario")
-        usuario.setValue("123", forKey: "senha")
-        usuario.setValue("Teste", forKey: "nome")
-        usuario.setValue(40, forKey: "idade")
+        produto.setValue("Ovos", forKey: "nome")
+        produto.setValue("descicao 3", forKey: "descricao")
+        produto.setValue("Vermelho", forKey: "cor")
+        produto.setValue(8.40, forKey: "preco")
         
         // Salvar valores no contexto(Requer uso do bloco: Try/Catch)
         do {
@@ -39,28 +37,53 @@ class ViewController: UIViewController {
             print("Erro ao salvar os dados")
         }
         */
-        /*
-            Recuperar dados utilizando Core Data
-        */
         
+        //    Recuperar dados utilizando Core Data
  
-        let requisicao = NSFetchRequest<NSFetchRequestResult>(entityName: "Usuarios")
-    
+        
+        
+        let requisicao = NSFetchRequest<NSFetchRequestResult>(entityName: "Produtos")
+        
+        //  Ordenar de A-Z
+        let ordenacaoAZ = NSSortDescriptor(key: "nome", ascending: true)
+        
+        //  Ordenar de Z-A
+        let ordenacaoZA = NSSortDescriptor(key: "preco", ascending: false)
+        
+        //  Aplicar Filtros
+        //let predicate = NSPredicate(format: "nome == %@", "Manteiga")
+        //let predicate = NSPredicate(format: "cor contains %@", "osa")
+        //let predicate = NSPredicate(format: "cor contains [c] %@", "rosa")
+        //let predicate = NSPredicate(format: "nome beginswith [c] %@", "Le")
+        let filtroPreco = NSPredicate(format: "preco >= @%", "4.0")
+        let filtroNome = NSPredicate(format: "nome == @% ", "Manteiga")
+        
+        let combinacaoFiltros = NSCompoundPredicate(andPredicateWithSubpredicates: [filtroPreco, filtroNome])
+        //let combinacaoFiltros = NSCompoundPredicate(orPredicateWithSubpredicates: filtroPreco, filtroNome)
+        
+        // Aplicar filtros e requisicao
+        requisicao.sortDescriptors = [ordenacaoAZ]
+        requisicao.predicate = combinacaoFiltros
+        
         do {
-            let usuarios = try context.fetch(requisicao)
-            if usuarios.count > 0 {
-                for usuario in usuarios {
-                    if let nomeUsuario = (usuario as AnyObject).value(forKey: "nome") {
-                        print(nomeUsuario)	
-                    }
+            let produtos = try context.fetch(requisicao)
+            if produtos.count > 0 {
+                for produto in produtos {
+                    let nomeProduto = (produto as AnyObject).value(forKey: "nome")
+                    let precoProduto = (produto as AnyObject).value(forKey: "preco")
+                    let corProduto = (produto as AnyObject).value(forKey: "cor")
+                    
+                    print(String(describing: nomeProduto) + "-" + String(describing: precoProduto))
                 }
             } else {
-                print("Nenhum usuario encontrado")
+                print("Nenhum produto encontrado")
             }
+            
+            print("Sucesso ao recuperar os produtos")
         } catch {
-            print("Erro ao recuperar ao dados!")
+            print("Erro ao recuperar os produtos")
         }
- 
+        
  
     }
 
