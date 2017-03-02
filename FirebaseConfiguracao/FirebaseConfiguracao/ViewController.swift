b1332f2ce7e538573ae2afce52b06b4d5b0260cc
Tiago Hermano <tiagofhermano@gmail.com>
2017-03-02 09:42:59 -0300

    Criado projeto Firebase: aprendendo conceitos de criação de listener para notificação de mudança de valores no banco de dados e criação e validação do cadastro de usuários

---
+++ FirebaseConfiguracao/FirebaseConfiguracao/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  FirebaseConfiguracao
+//
+//  Created by Tiago Hermano on 3/2/17.
+//  Copyright © 2017 Tiago Hermano. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textoLabel: UILabel!
+    let firebase = FIRDatabase.database().reference()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Criar Usuário com FireBase
+        FIRAuth.auth()?.createUser(withEmail: "dev13.seucondominio@gmail.com", password: "dev123456", completion: { (usuario, erro) in
+            if erro == nil {
+                print("Sucesso ao cadastrar o usuário")
+            } else {
+                print("Erro ao cadastrar usuario \(erro?.localizedDescription)")
+            }
+        })
+ 
+        // Verificar se usuário está logado(Ao criar usuário, este é automaticamente logado por padrão)
+        FIRAuth.auth()?.addStateDidChangeListener({ (FIRAuth, usuario) in
+            if let usuarioLogado = usuario {
+                print("Usuario está logado. Email: " + String(describing: usuarioLogado.email))
+            } else {
+                print("Usuario não está logado")
+            }
+        })
+        
+        /*
+        
+        let pontuacao = firebase.child("pontuacao").child("valor")
+        // pontuacao.removeValue()
+        // pontuacao.child("valor").setValue("100")
+        
+        // Recuperar dados do Firebase
+        pontuacao.observe(FIRDataEventType.value, with: {(dados) in
+            let ponto = dados.value as! String
+            self.textoLabel.text = ponto
+        })
+         */
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
