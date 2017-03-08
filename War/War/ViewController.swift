ca0429ee88c180596eed0c126175f34296c10d89
Tiago Hermano <tiagofhermano@gmail.com>
2017-03-08 11:11:43 -0300

    Criado outlets e implementada função de randomização de cartas

--- War/War/ViewController.swift
+++ War/War/ViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftImageView: UIImageView!
 
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var todasCartas:[String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.addTodasCartas()
+        print(todasCartas)
+    }
+    
+    func addTodasCartas() {
+        for i in 2 ..< 11 {
+            todasCartas.append("card\(i)")
+        }
+        todasCartas.append("jack")
+        todasCartas.append("queen")
+        todasCartas.append("king")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +39,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func dealTapped(_ sender: Any) {
+        //Randomize left number
+        let leftNumber = arc4random_uniform(UInt32(todasCartas.count))
+        
+        //Randomize right number
+        let rightNumber = arc4random_uniform(UInt32(todasCartas.count))
+        
+        leftImageView.image = UIImage(named: todasCartas[Int(leftNumber)])
+        rightImageView.image = UIImage(named: todasCartas[Int(rightNumber)])
+    }
 
 }
 
