c77b7c5fcdef22a3da341c29d31190bb1b6f9752
tiagohermano <tiagofhermano@gmail.com>
2017-07-10 11:18:11 -0300

    Iniciado e configurado app clone do Snapchat. Instalados e configurados pods do firebase e criadas as primeiras telas do app com autolayout

---
+++ Snapchat/Snapchat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snapchat
+//
+//  Created by iDev on 10/07/17.
+//  Copyright © 2017 iDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
