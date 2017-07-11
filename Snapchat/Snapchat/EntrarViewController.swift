d5c36e11089a8f543949380627e51e640eeff678
tiagohermano <tiagofhermano@gmail.com>
2017-07-11 09:20:58 -0300

    Terminado de construir interfaces das telas de Login e Cadastro e criação de suas respectivas classes

---
+++ Snapchat/Snapchat/EntrarViewController.swift
@@ -0,0 +1,39 @@
+//
+//  EntrarViewController.swift
+//  Snapchat
+//
+//  Created by iDev on 11/07/17.
+//  Copyright © 2017 iDev. All rights reserved.
+//
+
+import UIKit
+
+class EntrarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Snapchat/Snapchat/ViewController.swift
+++ Snapchat/Snapchat/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
