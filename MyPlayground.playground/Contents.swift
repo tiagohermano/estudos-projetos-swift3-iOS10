cd639be26ddb930a0fb186c51690f942c2bac0e9
tiagohermano <tiagofhermano@gmail.com>
2017-07-11 11:55:10 -0300

    Exclusão do projeto 'MyPlayground'

--- MyPlayground.playground/Contents.swift
+++
@@ -1,6 +0,0 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
-
-let latAleatoria = (Double(arc4random_uniform(500)) - 250) / 100000.0
-let longAleatoria = (Double(arc4random_uniform(500)) - 250) / 100000.0
