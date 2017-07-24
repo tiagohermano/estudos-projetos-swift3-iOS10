659f0a5d3f11aee6f22c9476db3191cb73c61883
tiagohermano <tiagofhermano@gmail.com>
2017-07-24 11:45:11 -0300

    Construido o funcionamento visual do launcher de opções

---
+++ YouTube/YouTube/SettingCell.swift
@@ -0,0 +1,60 @@
+//
+//  SettingCell.swift
+//  YouTube
+//
+//  Created by iDev on 21/07/17.
+//  Copyright © 2017 iDev. All rights reserved.
+//
+
+import UIKit
+
+class SettingCell: BaseCell {
+    
+    override var isHighlighted: Bool {
+        didSet {
+            backgroundColor = isHighlighted ? UIColor.darkGray : UIColor.white
+            nameLabel.textColor = isHighlighted ? UIColor.white : UIColor.black
+            
+            iconImageView.tintColor = isHighlighted ? UIColor.white : UIColor.darkGray
+        }
+    }
+    
+    var setting: Setting? {
+        didSet {
+            nameLabel.text = setting?.name
+            
+            if let imageName = setting?.imageName {
+                iconImageView.image = UIImage(named: imageName)?.withRenderingMode(.alwaysTemplate)
+                iconImageView.tintColor = UIColor.darkGray
+            }
+        }
+    }
+    
+    let nameLabel:UILabel = {
+        let label = UILabel()
+        label.text = "Settings"
+        label.font = UIFont.systemFont(ofSize: 13)
+        return label
+    }()
+    
+    let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "gear")
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+
+    override func setupViews() {
+        super.setupViews()
+        
+        addSubview(nameLabel)
+        addSubview(iconImageView)
+        
+        addConstraintsWithFormat(format: "H:|-8-[v0(30)]-8-[v1]", views: iconImageView, nameLabel)
+        addConstraintsWithFormat(format: "V:|[v0]|", views: nameLabel)
+        
+        addConstraintsWithFormat(format: "V:[v0(30)]", views: iconImageView)
+        
+        addConstraint(NSLayoutConstraint(item: iconImageView, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0))
+    }
+}
--- YouTube/YouTube/SettingsLauncher.swift
+++ YouTube/YouTube/SettingsLauncher.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
-class SettingsLauncher: NSObject {
+class Setting: NSObject {
+    let name: String
+    let imageName: String
+    
+    init(name: String, imageName: String) {
+        self.name = name
+        self.imageName = imageName
+    }
+}
+
+class SettingsLauncher: NSObject, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     let blackView = UIView()
     
-    let collectionView: UICollectionView  = {
+    let cellId = "cellId"
+    let cellHeight = 50
+    
+    let settings: [Setting] = {
+        return [Setting(name: "Settings", imageName: "gear"), Setting(name: "Terms & Privacy Policy", imageName: "lock"),
+                Setting(name: "Send feedback", imageName: "alert"), Setting(name: "Help", imageName: "help"),
+                Setting(name: "Switch Account", imageName: "user_profile"), Setting(name: "Cancel", imageName: "close")]
+    }()
+    
+    lazy var collectionViewSettings: UICollectionView  = {
         let layout = UICollectionViewFlowLayout()
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
         cv.backgroundColor = .white
@@ -29,11 +48,11 @@ class SettingsLauncher: NSObject {
             
             window.addSubview(blackView)
             
-            window.addSubview(collectionView)
+            window.addSubview(collectionViewSettings)
             
-            let height: CGFloat = 200
+            let height: CGFloat = CGFloat(settings.count * cellHeight)
             let y = window.frame.height - height
-            collectionView.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: 200)
+            collectionViewSettings.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: height)
             
             blackView.frame = window.frame
             blackView.alpha = 0
@@ -41,7 +60,7 @@ class SettingsLauncher: NSObject {
             UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: { 
                 self.blackView.alpha = 1
                 
-                self.collectionView.frame = CGRect(x: 0, y: y, width: self.collectionView.frame.width, height: self.collectionView.frame.height)
+                self.collectionViewSettings.frame = CGRect(x: 0, y: y, width: self.collectionViewSettings.frame.width, height: self.collectionViewSettings.frame.height)
             }, completion: nil)
             
         }
@@ -52,12 +71,38 @@ class SettingsLauncher: NSObject {
             self.blackView.alpha = 0
             
             if let window = UIApplication.shared.keyWindow {
-                self.collectionView.frame = CGRect(x: 0, y: window.frame.height, width: self.collectionView.frame.width, height: self.collectionView.frame.height)
+                self.collectionViewSettings.frame = CGRect(x: 0, y: window.frame.height, width: self.collectionViewSettings.frame.width, height: self.collectionViewSettings.frame.height)
             }
         }
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return settings.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! SettingCell
+        
+        let setting = settings[indexPath.item]
+        cell.setting = setting
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionViewSettings.frame.width, height: 50)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
     override init() {
         super.init()
+        collectionViewSettings.dataSource = self
+        collectionViewSettings.delegate = self
+        
+        collectionViewSettings.register(SettingCell.self, forCellWithReuseIdentifier: cellId)
     }
+    
 }
