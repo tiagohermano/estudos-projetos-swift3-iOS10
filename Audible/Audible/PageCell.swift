f14e3d0f6ff7c5d8019975b91e8ee6d4d87ececb
Tiago Hermano <tiagofhermano@gmail.com>
2017-09-05 12:38:59 -0300

    Criado models para exibição de páginas em um collectionView e setado suas respectivas views

--- Audible/Audible/PageCell.swift
+++ Audible/Audible/PageCell.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class PageCell: UICollectionViewCell {
     
+    var page:Page? {
+        didSet {
+            
+            guard let page = page else {
+                return
+            }
+            
+            imageView.image = UIImage(named: page.imageName)
+            
+            let color = UIColor(white: 0.2, alpha: 1)
+            
+            let attributedText = NSMutableAttributedString(string: page.title, attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 20, weight: UIFontWeightMedium), NSForegroundColorAttributeName: color])
+            
+            attributedText.append(NSAttributedString(string: "\n\n\(page.message)", attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 14), NSForegroundColorAttributeName: color]))
+            
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.alignment = .center
+            
+            let length = attributedText.string.characters.count
+            attributedText.addAttribute(NSParagraphStyleAttributeName, value: paragraphStyle, range: NSRange(location: 0, length: length))
+            
+            textView.attributedText = attributedText
+        }
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -30,7 +55,14 @@ class PageCell: UICollectionViewCell {
         let tv = UITextView()
         tv.text = "SAMPLE TEXT FOR NOW"
         tv.isEditable = false
+        tv.contentInset = UIEdgeInsets(top: 24, left: 0, bottom: 0, right: 0)
         return tv
+    }()
+    
+    let lineSeparatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0.9, alpha: 1)
+        return view
     }()
     
     required init?(coder aDecoder: NSCoder) {
@@ -40,11 +72,16 @@ class PageCell: UICollectionViewCell {
     func setupViews() {
         addSubview(imageView)
         addSubview(textView)
+        addSubview(lineSeparatorView)
         
         imageView.anchorToTop(top: topAnchor, left: leftAnchor, bottom: textView.topAnchor, right: rightAnchor)
         
-        textView.anchorToTop(top: nil, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor)
+        textView.anchorWithConstantsToTop(top: nil, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 16, bottomConstant: 0, rightConstant: 16)
+        
         textView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.3).isActive = true
+        
+        lineSeparatorView.anchorToTop(top: nil, left: leftAnchor, bottom: textView.topAnchor, right: rightAnchor)
+        lineSeparatorView.heightAnchor.constraint(equalToConstant: 1).isActive = true
     }
     
 }
--- Audible/Audible/ViewController.swift
+++ Audible/Audible/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         layout.minimumLineSpacing = 0
         
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        cv.backgroundColor = .red
+        cv.backgroundColor = .white
         
         cv.dataSource = self
         cv.delegate = self
@@ -30,6 +30,14 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }()
     
     let cellId = "cellId"
+    
+    let pages: [Page] = {
+        let firstPage = Page(title: "Share a great listen", message: "It's free to send your books to the people in your life. Every recipient's first book is on us.", imageName: "page1")
+        let secondPage = Page(title: "Send from your library", message: "Tap the More menu next to any book. Choose \"Send this Book\"", imageName: "page2")
+        let thirdPage = Page(title: "Send from the player", message: "Tap the More menu in the upper corner. Choose \"Send this Book\"", imageName: "page3")
+        
+        return [firstPage, secondPage, thirdPage]
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,11 +57,13 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
+        return pages.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! PageCell
+        let page = pages[indexPath.item]
+        cell.page = page
         
         return cell
     }
