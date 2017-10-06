e12b28a7d06b376e6876138c1654e976d8abf809
Tiago Hermano <tiagofhermano@gmail.com>
2017-10-06 12:17:43 -0300

    Última atualização do projeto Snap Behavior

--- Snap-Behavior/Snap-Behavior/ViewController.swift
+++ Snap-Behavior/Snap-Behavior/ViewController.swift
@@ -31,6 +31,49 @@ class ViewController: UIViewController {
             animator.addBehavior(snap)
         }
     }
+    
+    @IBAction func btn1Pressed(_ sender: Any) {
+        animator.removeAllBehaviors()
+        
+        let position = CGPoint(x: self.view.frame.width / 2, y: (sender as AnyObject).frame.height)
+        snap = UISnapBehavior(item: sender as! UIButton, snapTo: position)
+        snap.damping = 0.18
+        
+        animator.addBehavior(snap)
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        animator.removeAllBehaviors()
+        
+        for touch in touches {
+            let location = touch.location(in: self.view)
+            
+            let button = Buttons[1]
+            
+            snap = UISnapBehavior(item: button, snapTo: location)
+            snap.damping = 0.18
+            
+            animator.addBehavior(snap)
+            
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        animator.removeAllBehaviors()
+        
+        for touch in touches {
+            let location = touch.location(in: self.view)
+            
+            let button = Buttons[1]
+            
+            snap = UISnapBehavior(item: button, snapTo: location)
+            snap.damping = 0.2
+            
+            animator.addBehavior(snap)
+            
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
